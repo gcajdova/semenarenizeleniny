e43b155e775f05a59107b56e560b7a0ae193a4b9
gcajdova <305gabca96@gmail.com>
2026-09-21 20:50:35 +0200

    Přidat aplikaci Semínka: public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'seminka-static-v1';
+const STATIC_ASSETS = ['./', 'assets/app.css', 'assets/app.js', 'assets/icon.svg', 'manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((names) => Promise.all(names.filter((name) => name !== CACHE).map((name) => caches.delete(name)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
